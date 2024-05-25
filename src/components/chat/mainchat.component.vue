96f52f4230f34e32f9fed4b6faf62ad64741de23
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-05-26 00:46:46 +1000

    main chat with user input up

--- src/components/chat/mainchat.component.vue
+++ src/components/chat/mainchat.component.vue
@@ -1,20 +1,43 @@
 <template>
-  <div class="d-flex flex-column gap-3 h-100 w-75 mx-auto">
+  <div
+    class="d-flex flex-column gap-3 h-100 w-75 mx-auto"
+    @keypress.enter="sendMessage"
+  >
+    <!-- Display user inputs -->
+    <div class="mt-3">
+      <div v-for="(message, index) in messages" :key="index" class="user-message d-flex flex-row gap-4">
+      <div
+        style="height: 30px; width: 30px; object-fit: cover"
+        class="rounded-circle overflow-hidden d-flex justify-content-center align-items-center"
+      >
+        <img
+          :src="userImage.getUserInfo.image"
+          alt="portrait"
+          style="width: 100%; height: 100%; object-fit: cover"
+        />
+      </div>
+
+       {{ message }}
+    </div>
+    </div>
+    
     <!-- ai chat and response box -->
     <div class="flex-grow-1">
-      <WelcomeChatComponent />
+      <WelcomeChatComponent v-if="messages.length === 0" />
     </div>
     <!-- user input -->
     <div class="input-container">
       <div class="mb-2 position-relative">
         <input
-          type="email"
+          v-model="userInput"
+          type="text"
           class="form-control py-3 px-2 border-warning"
           id="floatingInput"
           placeholder="ask any question here...."
         />
         <div class="pl-2">
           <button
+            @click="sendMessage"
             class="position-absolute top-50 end-0 translate-middle-y bg-transparent border-0"
           >
             <SendIcon />
@@ -26,8 +49,20 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import SendIcon from "../icon/send.icon.vue";
 import WelcomeChatComponent from "./welcome.chat.component.vue";
+import { useUserInforStore } from "@/stores/userInfor";
+
+const userInput = ref("");
+const messages = ref([]);
+const userImage = useUserInforStore();
+const sendMessage = () => {
+  if (userInput.value.trim() !== "") {
+    messages.value.push(userInput.value);
+    userInput.value = "";
+  }
+};
 </script>
 
 <style scoped>
@@ -42,5 +77,12 @@ import WelcomeChatComponent from "./welcome.chat.component.vue";
 .form-control:focus {
   outline: none;
   box-shadow: none;
+}
+
+.user-message {
+  text-align: start;
+  margin: 3px;
+  border-radius: 5px;
+  margin-bottom: 20px;
 }
 </style>
