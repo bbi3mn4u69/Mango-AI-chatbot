cddd0d47a25efbfb47b14febbfabacad6b07f353
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-05-25 22:11:23 +1000

    update main chat UI

--- src/components/chat/welcome.chat.component.vue
+++ src/components/chat/welcome.chat.component.vue
@@ -3,12 +3,12 @@
         <div
           class="d-flex flex-column justify-content-center align-items-start py-5"
         >
-          <div class="mt-5 mb-4 text-start fs-3 fw-bold">
+          <div class="mt-5 mb-4 text-start sub-text">
             <div
               class="d-flex flex-column text-center justify-content-center align-items-center"
             >
-              <span class="fw-bold text-warning">
-                Hi, I'm your AI assistant
+              <span class="fw-bold gradient-text">
+                Hi, {{ userInfor.getUserInfo.name }}.
               </span>
               What can I help you with today?
             </div>
@@ -17,9 +17,22 @@
       </div>
 </template>
 
-<script>
-    export default {
-        name: 'welcome-chat'
-    }
+<script setup>
+    import { useUserInforStore } from "@/stores/userInfor";
+    const userInfor = useUserInforStore();
 </script>
 
+<style scoped>
+.gradient-text {
+  background: linear-gradient(90deg, #ffcc00, #ff6699);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 3rem; /* Adjust the font size as needed */
+  font-weight: bold; /* Adjust the font weight as needed */
+}
+.sub-text {
+  color: #a3a3a3;
+  font-size: 2.3rem;
+  font-weight: bold;
+}
+</style>
