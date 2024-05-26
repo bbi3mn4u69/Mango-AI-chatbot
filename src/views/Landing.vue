8a825e60d6ee5fb036d7d7bbed68f207dd0c2094
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-05-27 06:28:37 +1000

    add credit in landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -6,11 +6,12 @@
     <FeedbackSection />
     <FormSection />
     <LastCallSection />
+    <Credit/>
     <Footer/>
   </div>
 </template>
 
-<script>
+<script setup>
 import FeedbackSection from "@/components/landing/feedback.section.component.vue";
 import HeroSection from "@/components/landing/hero.section.componenr.vue";
 import IntroductionSection from "@/components/landing/introduction.section.component.vue";
@@ -18,16 +19,5 @@ import FormSection from "@/components/landing/form.section.component.vue";
 import LastCallSection from "@/components/landing/lastcall.section.component.vue";
 import Navbar from "@/components/shared/navbar.shared.component.vue";
 import Footer from "@/components/shared/footer.shared.component.vue"
-export default {
-  name: "LandingPage",
-  components: {
-    FeedbackSection,
-    HeroSection,
-    IntroductionSection,
-    FormSection,
-    LastCallSection,
-    Navbar,
-    Footer
-  },
-};
+import Credit from "@/components/shared/credit.shared.component.vue"
 </script>
