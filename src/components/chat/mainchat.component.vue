65d75f99a55ec16faba205ba770fcdad239f3156
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-05-26 16:59:15 +1000

    intergrate chat

--- src/components/chat/mainchat.component.vue
+++ src/components/chat/mainchat.component.vue
@@ -5,25 +5,38 @@
   >
     <!-- Display user inputs -->
     <div ref="messageDisplay"  class="mt-3 user-input-display">
-      <div v-for="(message, index) in messages" :key="index" class="user-message d-flex flex-row gap-4">
+      <div v-for="(chat, index) in chatStore.chats" :key="index" class="user-message d-flex flex-row gap-4">
       <div
-        style="height: 30px; width: 30px; object-fit: cover"
+        style="height: 45px; width: 45px; object-fit: cover"
         class="rounded-circle overflow-hidden d-flex justify-content-center align-items-center"
       >
         <img
-          :src="userImage.getUserInfo.image"
+          :src="chat.image"
           alt="portrait"
           style="width: 100%; height: 100%; object-fit: cover"
         />
       </div>
 
-       {{ message }}
+       {{ chat.message }}
+    </div>
+    <div v-if="isLoading === true" class="user-message d-flex flex-row gap-4">
+      <div
+        style="height: 45px; width: 45px; object-fit: cover"
+        class="rounded-circle overflow-hidden d-flex justify-content-center align-items-center border"
+      >
+        <img
+          src="/logo/logo.png"
+          alt="portrait"
+          style="width: 100%; height: 100%; object-fit: cover"
+        />
+      </div>
+      <div class="loader"></div>
     </div>
     </div>
     
     <!-- ai chat and response box -->
     <div class="flex-grow-1">
-      <WelcomeChatComponent v-if="messages.length === 0" />
+      <WelcomeChatComponent v-if="chatStore.chats.length === 0" />
     </div>
     <!-- user input -->
     <div class="input-container">
@@ -34,6 +47,7 @@
           class="form-control py-3 px-2 border-warning"
           id="floatingInput"
           placeholder="ask any question here...."
+          autocomplete="off"
         />
         <div class="pl-2">
           <button
@@ -53,14 +67,25 @@ import { ref, nextTick } from "vue";
 import SendIcon from "../icon/send.icon.vue";
 import WelcomeChatComponent from "./welcome.chat.component.vue";
 import { useUserInforStore } from "@/stores/userInfor";
+import run from "@/lib/AIService";
+import { useChatStore } from "@/stores/chat";
 
 const userInput = ref("");
-const messages = ref([]);
 const userImage = useUserInforStore();
 const messageDisplay = ref(null);
-const sendMessage = () => {
+const chatStore = useChatStore();
+let isLoading = ref(false);
+const sendMessage = async () => {
+  isLoading = true
+  if(userInput.value ){
+    chatStore.addChat({whomChat: "user", image: userImage.getUserInfo.image, message: userInput.value});
+    const response = await run(userInput.value)
+     if(response){
+      isLoading = false
+      chatStore.addChat({whomChat: "ai", image: "/logo/logo.png", message: response});
+     }
+  }
   if (userInput.value.trim() !== "") {
-    messages.value.push(userInput.value);
     userInput.value = "";
     nextTick(() => {
       if (messageDisplay.value) {
@@ -92,8 +117,31 @@ const sendMessage = () => {
   border-radius: 5px;
   margin-bottom: 20px;
 }
+
 .user-input-display {
   max-height: 600px; 
   overflow-y: scroll;
 }
+
+.loader {
+  width: 30px;
+  aspect-ratio: 2;
+  --_g: no-repeat radial-gradient(circle closest-side,#000 90%,#0000);
+  background: 
+    var(--_g) 0%   50%,
+    var(--_g) 50%  50%,
+    var(--_g) 100% 50%;
+  background-size: calc(100%/3) 50%;
+  animation: l3 1s infinite linear;
+}
+
+@keyframes l3 {
+    20%{background-position:0%   0%, 50%  50%,100%  50%}
+    40%{background-position:0% 100%, 50%   0%,100%  50%}
+    60%{background-position:0%  50%, 50% 100%,100%   0%}
+    80%{background-position:0%  50%, 50%  50%,100% 100%}
+}
+
 </style>
+
+
