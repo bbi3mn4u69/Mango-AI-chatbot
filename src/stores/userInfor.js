f68b74c2a254f02d6a318c4ef422ec5df2bcc5d0
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-05-28 18:23:12 +1000

    update user infor

--- src/stores/userInfor.js
+++ src/stores/userInfor.js
@@ -8,16 +8,18 @@ export const useUserInforStore = defineStore("userInfor", {
     },
   }),
   actions: {
-    setUserInfo(name, email, image) {
+    setUserInfo(name, email, image, authRole) {
       this.user.name = name;
       this.user.email = email;
       this.user.image = image;
+      this.user.authRole = authRole;
     },
     resetUserInfo() {
       this.user.name = null;
       this.user.email = null;
       this.user.image = null;
-    }
+      this.user.authRole = null;
+    },
   },
   getters: {
     getUserInfo: (state) => state.user
