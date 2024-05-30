736535ff4d4c08d0ccc297d95f072073dfd614dd
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-05-31 03:00:34 +1000

    add check icon

---
+++ src/components/icon/check.icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="6"
+    stroke="#fafafa"
+    style="width: 20px; height: 20px"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="m4.5 12.75 6 6 9-13.5"
+    />
+  </svg>
+</template>
