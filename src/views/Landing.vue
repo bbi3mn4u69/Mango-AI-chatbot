1e90c7ddea3078b5e369cc78e354113d54fe7bc7
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-01 02:16:38 +1000

    update auto fetch

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,10 +3,9 @@
     <Navbar />
     <HeroSection />
     <IntroductionSection />
-    <FeedbackSection />
-    <FormSection />
+    <FeedbackSection ref="feedbackSection"/>
+    <FormSection  @feedback-submitted="fetchFeedbacks"/>
     <LastCallSection />
-    
     <Footer/>
     <Credit/>
   </div>
@@ -21,4 +20,12 @@ import LastCallSection from "@/components/landing/lastcall.section.component.vue
 import Navbar from "@/components/shared/navbar.shared.component.vue";
 import Footer from "@/components/shared/footer.shared.component.vue"
 import Credit from "@/components/shared/credit.shared.component.vue"
+import { ref } from 'vue';
+const feedbackSection = ref(null);
+
+const fetchFeedbacks = () => {
+  if (feedbackSection.value) {
+    feedbackSection.value.fetchData();
+  }
+};
 </script>
